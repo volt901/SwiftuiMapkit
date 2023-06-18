2c92a4da666aa5d830a37ad677b75e1a81d42da4
volt901 <volt901@icloud.com>
2023-06-18 12:28:05 +0500

    Initial Commit

---
+++ SwiftuiMapkit/SwiftuiMapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapkitApp.swift
+//  SwiftuiMapkit
+//
+//  Created by Владимир Ширяев on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
